84d99333a861da549f9cdb5778a46728cd1aab25
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-04 17:19:25 +0300

    hotfix: fix lint errors

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
+const indexRouter = require('express').Router();
+
 const cardRouter = require('./card');
 const signRouter = require('./sign');
 const userRouter = require('./user');
 
 const { pathNotFound } = require('../consts/errorMessages');
 const NotFoundError = require('../errors/NotFoundError');
-
-const indexRouter = require('express').Router();
 
 indexRouter.use('/', signRouter);
 indexRouter.use('/users', userRouter);
