386e3496b33d216f3e6aee6a8deb3ebd17812a2e
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-07 17:01:16 +0300

    feat: add logger & cors

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errors } = require('celebrate');
 
 const error = require('./middlewares/error');
 const indexRouter = require('./routes');
 const { app, PORT } = require('./config');
+const checkCors = require('./middlewares/checkCors');
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
+app.use(checkCors);
 
-app.use('/', indexRouter);
+app.use('/api/', indexRouter);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
 app.listen(PORT);
--- config.js
+++ config.js
@@ -8,7 +8,14 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
+const allowedCors = [
+  'http://kznv.alex.nomoreparties.co',
+  'https://kznv.alex.nomoreparties.co',
+  'localhost:3000',
+];
+
 module.exports = {
   PORT,
   app,
+  allowedCors,
 };
